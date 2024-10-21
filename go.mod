9ac15f78bc8b11de2fd689f85061a35a44d1ff7e
Paulus Otto Harman <lonewolfffffff@gmail.com>
2024-10-21 10:17:13 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/paulus-otto-harman/golang-module-test1
+
+go 1.23.2
---
+++ log.go
@@ -0,0 +1,7 @@
+package paul
+
+import "fmt"
+
+func Logger(teks string) string {
+	return fmt.Sprintf("Paul : %s", teks)
+}
